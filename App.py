e6a4470171b5196368f29806980c912bea918309
anlitop <anlitop@163.com>
2019-11-06 19:50:15 +0800

    按钮事件

--- App.py
+++ App.py
@@ -13,7 +13,27 @@ class App(QMainWindow,Ui_Battle):
         super(App,self).__init__()
         self.setupUi(self)
         #绑定事件
+        self.select_type.addItems(["次留玩家","单日玩家","我全都要"])#config
+        self.select_type.activated[str].connect(self.OnChooseType)
+        #复制sql
+        self.copy_sql_btn.clicked.connect(self.OnClickCopyBtn)
+        #选择账号文件
+        self.select_battle_path.connect(self.OnClickCopyBtn)
+        #选择战斗记录文件
+        self.select_account_btn.connect(self.OnClickCopyBtn)
 
+
+
+    def OnChooseType(self,text):
+        #测试
+        msgbox=QMessageBox(QMessageBox.NoIcon,text,text)
+        msgbox.exec()
+    
+    def OnClickCopyBtn(self):
+        #获取date的时间
+        #修改config
+        #将config的复制到剪切板
+        pass
 
 
 
